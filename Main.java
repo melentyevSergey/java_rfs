243d1fe52bec045d0c025ab998928cd3c7d2f587
melentyevSergey <109089067+melentyevSergey@users.noreply.github.com>
2023-04-13 23:11:04 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,37 @@
-// Аргументы — конкретные значения параметров функции
+https://github.com/ka5trulkin
+
+@PathVariable
+https://www.baeldung.com/spring-pathvariable
+
+@RequestParam 
+https://www.baeldung.com/spring-request-param
+
+@RequestBody
+https://www.baeldung.com/spring-request-response-body
+
+// тренажер git
+https://learngitbranching.js.org/?locale=ru_RU
+
+// валидация в spring boot
+https://struchkov.dev/blog/ru/spring-boot-validation/
+
+// PostsgreSQL cert
+https://www.enterprisedb.com/training
+
+// data Structures
+https://www.youtube.com/playlist?list=PLpPXw4zFa0uKKhaSz87IowJnOTzh9tiBk
+
+// spring потрошитель
+https://www.youtube.com/playlist?list=PL6yLoZ_3Y0HKGL3F7vv2SNSrA3TkbXtBX
+
+// kafka
+https://www.youtube.com/watch?v=-AZOi3kP9Js
+
+
+// Нормализация БД
+https://youtu.be/zqQxWdTpSIA
+
+
 
 
 
@@ -21,7 +54,7 @@ import java.util.Scanner;
 
 Scanner scanner = new Scanner(System.in);
 String command = scanner.nextLine();
-int age = Integer.parseInt(scanner.readLine());
+
 
 
 
@@ -207,7 +240,7 @@ Collections.disjoint(arrayListName1, arrayListName2); // true if there are no sa
 
 
 /*
-    ARRAY
+    LIST
 */
 
 https://docs.oracle.com/javase/7/docs/api/java/util/Collections.html
@@ -221,6 +254,9 @@ solarSystem.add(str3);  // Exception in thread "main" java.lang.UnsupportedOpera
 
 // swap values
 Collections.swap(arrayListName, arrayListName.indexOf(str1), arrayListName.indexOf(str2));
+
+ArrayList<Integer> listAllId = new ArrayList<>();
+Collections.max(listAllId);
 
 
 
@@ -336,19 +372,14 @@ public class FortuneCookieFactoryTest {
     }
 }
 
-
-class ExceptionTest {
-    @Test
-    void shouldThrowException() {
-        final ArithmeticException exception = assertThrows(
-                ArithmeticException.class,
-                new Executable() {
-                    @Override
-                    public void execute() { int number = 10 / 0; }
-                });
-        assertEquals("/ by zero", exception.getMessage());
-    }
+@Test
+void shouldThrowException() {
+    final ArithmeticException exception = assertThrows(
+            ArithmeticException.class,
+            () -> { int number = 10 / 0; });
+    assertEquals("/ by zero", exception.getMessage());
 }
+
 
 
 
@@ -413,4 +444,203 @@ public static long getInsertTimeInMs(List list) {
     anyLongOperation(list);
     long elapsedTime = System.nanoTime() - startTime;
     return elapsedTime / 1000000;
+}
+
+
+
+
+/*
+    GSON
+*/
+
+String lastLikeInfoStr = "{ \"user\": \"Алексей\", \"hours\": 12, \"minutes\": 30}";
+Gson gson = new Gson();
+LastLikeInfo lastLikeInfo = gson.fromJson(lastLikeInfoStr, LastLikeInfo.class);
+
+LikesInfo likesInfo = new LikesInfo();
+String likesInfoSerialized = gson.toJson(likesInfo);
+
+GsonBuilder gsonBuilder = new GsonBuilder();
+gsonBuilder.serializeNulls();
+Gson gson = gsonBuilder.create();
+
+class LocalDateAdapter extends TypeAdapter<LocalDate> {
+    private static final DateTimeFormatter formatterWriter = DateTimeFormatter.ofPattern("dd--MM--yyyy");
+    private static final DateTimeFormatter formatterReader = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    @Override
+    public void write(final JsonWriter jsonWriter, final LocalDate localDate) throws IOException {
+        jsonWriter.value(localDate.format(formatterWriter));
+    }
+
+    @Override
+    public LocalDate read(final JsonReader jsonReader) throws IOException {
+        return LocalDate.parse(jsonReader.nextString(), formatterReader);
+    }
+}
+GsonBuilder gsonBuilder = new GsonBuilder();
+gsonBuilder.registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
+Gson gson = gsonBuilder.create();
+
+
+
+
+/*
+    HTTPServer
+*/
+
+private static final int PORT = 8080;
+public static void main(String[] args) throws IOException {
+    HttpServer httpServer = HttpServer.create();
+    httpServer.bind(new InetSocketAddress(PORT), 0);
+    httpServer.createContext("/hello", new HelloHandler());
+    httpServer.start();
+}
+static class HelloHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String method = httpExchange.getRequestMethod();
+        
+        Headers requestHeaders = httpExchange.getRequestHeaders();
+        List<String> contentTypeValues = requestHeaders.get("Content-type");
+        if ((contentTypeValues != null) && (contentTypeValues.contains("application/json"))) {
+            System.out.println("Это JSON!");
+        }
+
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", "text/plain");
+        headers.set("X-your-own-header", "any-information-you-want");
+        httpExchange.sendResponseHeaders(200, 0);
+
+        InputStream inputStream = httpExchange.getRequestBody();
+        String body = new String(inputStream.readAllBytes(), DEFAULT_CHARSET);
+
+        String path = httpExchange.getRequestURI().getPath();
+        String name = path.split("/")[2];
+        String response = "Привет, " + name + "!";
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
+    }
+}
+
+
+
+
+/*
+    URI
+*/
+
+public static void main(String[] args) {
+    HttpClient client = HttpClient.newHttpClient();
+
+    // укажите URL запроса, включая его параметры
+    URI url = URI.create("https://api.exchangerate.host/convert?base=RUB&symbols=USD");
+
+    // создайте объект, описывающий запрос с необходимой информацией
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(url)
+            .header("Accept", "application/json")
+            .GET()
+            .build();
+
+    try {
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Код статуса: " + response.statusCode());
+        System.out.println("Ответ: " + response.body());
+
+        if (response.statusCode() == 200) {
+            JsonElement jsonElement = JsonParser.parseString(response.body());
+            if(!jsonElement.isJsonObject()) { // проверяем, точно ли мы получили JSON-объект
+                System.out.println("Ответ от сервера не соответствует ожидаемому.");
+                return;
+            }
+            // получите курс доллара и евро и запишите в переменные rateUSD и rateEUR
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            JsonObject ratesObj = jsonObject.getAsJsonObject("rates");
+
+            double rateUSD = ratesObj.get("USD").getAsDouble();
+            double rateEUR = ratesObj.get("EUR").getAsDouble();
+        } else {
+            System.out.println("Сервер вернул код состояния: " + response.statusCode());
+        }
+    } catch (IOException | InterruptedException e) { // обработка ошибки отправки запроса
+        System.out.println("Во время выполнения запроса '" + url + "' возникла ошибка.\n" +
+                "Проверьте, пожалуйста, адрес и повторите попытку.");
+    }
+}
+
+
+
+/*
+    LOGGER
+*/
+
+TRACE (англ. «отслеживать») — для трассировочных сообщений,
+которые предоставляют очень подробную информацию о каком-либо процессе. 
+DEBUG (англ. «отлаживать») — для отладочных сообщений,
+которые важны во время анализа некорректного поведения программы
+INFO (англ. «информация») — для сообщений,
+которые помогают понять текущее состояние программы.
+WARN (англ. «предупреждать») — для сообщений с предупреждениями,
+которые появляются в случае, если что-то пошло не так.
+ERROR (англ. «ошибка») — для сообщений о критических ошибках, требующих оперативного вмешательства,
+без которого программа не сможет продолжить работу.
+
+// Уровень логгера по пакетам
+application.properties
+logging.level.ru.yandex.practicum=debug
+logging.level.org.springframework.web.servlet.DispatcherServlet=debug
+
+// инициализация логгера и использование
+private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
+
+log.info("Получен запрос.");
+log.debug("Текущее количество постов: {}", posts.size());
+
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-validation</artifactId>
+</dependency> 
+https://docs.jboss.org/hibernate/beanvalidation/spec/2.0/api/javax/validation/constraints/package-summary.html
+
+
+
+/*
+    LOMBOK
+    https://projectlombok.org/features/
+*/
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Point {
+    private int x;
+    private int y;
+} 
+
+
+
+/*
+    SPRING
+*/
+
+@ControllerAdvice("ru.yandex.practicum.controllers")
+@ControllerAdvice(assignableTypes = {DogsInteractionController.class, CatsInteractionController.class})
+
+@GetMapping("/feed")
+public Map<String, Integer> methodName() {
+    throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED, "Метод ещё не реализован.");
+}
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Integer> handleOverflow(final OverflowException e) {
+        return Map.of("level", e.getLevel());
+    }
 }
